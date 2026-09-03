21749c09905d074bf8e3a21e9b0648732873d0d3
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-03 15:34:29 -0700

    feat: add get product to controller

--- src/modules/products/productController.ts
+++ src/modules/products/productController.ts
@@ -1,5 +1,5 @@
 import {Request,Response,NextFunction} from "express";
-import { getProductsService } from "./productService.js";
+import { getProductService, getProductsService } from "./productService.js";
 import { AppError } from "../../errors/AppError.js";
 
 
@@ -11,6 +11,24 @@ export const getProcuts = async(
         try{
             const products = await getProductsService();
             res.status(200).json({products:products});
+        }catch(err){
+            if(err instanceof AppError){
+                if(!err.statusCode){
+                    err.statusCode = 500;
+                }
+            }
+            next(err);
+        }
+}
+
+export const getProduct = async(
+    req:Request,
+    res:Response,
+    next:NextFunction)=>{
+        try{
+            const productId = Number(req.params.id);
+            const product = await getProductService(productId);
+            res.status(200).json({product:product});
         }catch(err){
             if(err instanceof AppError){
                 if(!err.statusCode){
