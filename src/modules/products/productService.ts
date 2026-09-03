d73e55444f27ca1829abe4f6ab35aea9bdf02643
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-03 15:35:13 -0700

    feat: add get product service

--- src/modules/products/productService.ts
+++ src/modules/products/productService.ts
@@ -6,3 +6,10 @@ import {Prisma} from "../../generated/prisma/client.js";
 export const getProductsService = async()=>{
     return await prisma.product.findMany();
 }
+export const getProductService = async(productId:number)=>{
+    const product = await prisma.product.findUnique({where:{id:productId}});
+    if(!product){
+        throw new AppError("this product doesn't exist",404);
+    }
+    return product;
+}
