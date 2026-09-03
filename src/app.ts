1cd3099bce5dbee9b1746c2fb665bf9004b217fc
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-03 15:33:53 -0700

    style: format app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,36 @@
 import express from "express";
 import cors from "cors";
+
 import categoryRouter from "./modules/categories/categoryRoutes.js";
 import productRouter from "./modules/products/productRoutes.js";
 import { AppError } from "./errors/AppError.js";
-
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/categories",categoryRouter);
-app.use("/products",productRouter);
 
-app.use((error:AppError,req:express.Request,res:express.Response,next:express.NextFunction)=>{
+
+// Routes
+app.use("/categories", categoryRouter);
+app.use("/products", productRouter);
+
+// Error handling — keep this LAST
+app.use(
+  (
+    error: AppError,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
     const errStatus = error.statusCode;
     const errMsg = error.message;
+
     console.log(error);
-    res.status(errStatus).json({errMsg:errMsg});
-});
 
-
+    res.status(errStatus).json({ errMsg });
+  }
+);
 
 export default app;
