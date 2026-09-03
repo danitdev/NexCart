241cb87e65439d67681d4ab1fdeb9e4e460b6cbb
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-02 19:45:12 -0700

    feat: add category POST service

--- src/modules/categories/categoryService.ts
+++ src/modules/categories/categoryService.ts
@@ -3,3 +3,11 @@ import {prisma} from "../../lib/prisma.js";
 export const getCategoriesService = async()=>{
     return await prisma.category.findMany();
 }
+
+export const postCategoryService = async(categoryName:string)=>{
+    const category = await prisma.category.create({data:{
+        name: categoryName
+    }});
+    return category;
+
+}
