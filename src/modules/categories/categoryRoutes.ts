c0eddb8572d9ab71bcf9e2369039936eb88e62db
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-02 19:52:09 -0700

    feat: add getCategory to the router

--- src/modules/categories/categoryRoutes.ts
+++ src/modules/categories/categoryRoutes.ts
@@ -1,10 +1,10 @@
 import {Router} from "express";
-import {getCategories, postCategory} from "./categoryController.js";
+import {getCategories, getCategory, postCategory} from "./categoryController.js";
 
 const router = Router();
 
 router.get("/",getCategories);
-router.get("/:id",);
+router.get("/:id",getCategory);
 router.post("/",postCategory);
 // router.patch("/:id",)
 // router.delete("/:id");
