f68ab40c98dd287f65c08b8052d86a782738c48d
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-03 14:42:53 -0700

    feat: add update category service

--- src/modules/categories/categoryService.ts
+++ src/modules/categories/categoryService.ts
@@ -1,4 +1,6 @@
 import {prisma} from "../../lib/prisma.js";
+import {AppError} from "../../errors/AppError.js";
+import {Prisma} from "../../generated/prisma/client.js";
 
 export const getCategoriesService = async()=>{
     return await prisma.category.findMany();
@@ -15,7 +17,20 @@ export const postCategoryService = async(categoryName:string)=>{
 export const getCategoryService = async(categoryId:number)=>{
     const category =  await prisma.category.findUnique({where:{id:categoryId}});
     if(!category){
-        throw new Error("This Shit doesn't exist!");
+        throw new AppError("This Shit doesn't exist!",404);
     }
     return category;
+}
+export const updateCategoryService = async(categoryId:number,categoryName:string)=>{
+    try{
+        return await prisma.category.update({
+            where:{id:categoryId},
+            data:{name:categoryName}
+        });
+    }catch(error){
+        if(error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2025"){
+            throw new AppError("Category not found.",404);
+        }
+        throw error;
+    }
 }
