ec39a236dc0c9e0fb78fe13478bd502993428825
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-02 18:25:02 -0700

    feat: add prisma adapter to be used in app

---
+++ src/lib/prisma.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import {PrismaClient} from "../generated/prisma/client.js";
+import {PrismaMariaDb} from "@prisma/adapter-mariadb";
+
+const adapter = new PrismaMariaDb({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+export const prisma = new PrismaClient({adapter});
