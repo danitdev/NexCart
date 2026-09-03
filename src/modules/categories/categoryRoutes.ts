fa9d503b3c2682fca2080b68bef5b9c869a2dc42
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-02 19:45:30 -0700

    feat: add routes for category POST

--- src/modules/categories/categoryRoutes.ts
+++ src/modules/categories/categoryRoutes.ts
@@ -1,8 +1,13 @@
 import {Router} from "express";
-import {getCategories} from "./categoryController.js";
+import {getCategories, postCategory} from "./categoryController.js";
 
 const router = Router();
 
 router.get("/",getCategories);
+router.get("/:id",);
+router.post("/",postCategory);
+// router.patch("/:id",)
+// router.delete("/:id");
+
 
 export default router;
