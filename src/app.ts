9d933ab2a3c9281e4e4108b88c61ee3f08d91f8a
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-02 19:36:13 -0700

    feat: add categoryRouter to the app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import categoryRouter from "./modules/categories/categoryRoutes.js";
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/categories",categoryRouter);
 
 
 export default app;
