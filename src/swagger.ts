570dadea1496ec1c687a0ef529661784a8c8daa4
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-03 15:34:14 -0700

    feat: add swagger ui settings

---
+++ src/swagger.ts
@@ -0,0 +1,13 @@
+import swaggerUi from "swagger-ui-express";
+
+const swaggerDocument = {
+  openapi: "3.0.0",
+  info: {
+    title: "NexCart API",
+    version: "1.0.0",
+    description: "E-commerce REST API",
+  },
+  paths: {},
+};
+
+export default swaggerDocument;
