bc6369a2fc02bf3ae1f7274b47e782df7a4d43a9
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-03 14:41:55 -0700

    feat: add error handling to app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import categoryRouter from "./modules/categories/categoryRoutes.js";
+import { AppError } from "./errors/AppError.js";
 
 
 const app = express();
@@ -9,6 +10,14 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/categories",categoryRouter);
+
+app.use((error:AppError,req:express.Request,res:express.Response,next:express.NextFunction)=>{
+    const errStatus = error.statusCode;
+    const errMsg = error.message;
+    console.log(error);
+    res.status(errStatus).json({errMsg:errMsg});
+});
+
 
 
 export default app;
