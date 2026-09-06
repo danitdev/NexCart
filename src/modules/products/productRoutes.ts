f9b18a46750c69d367d71240f15590e0e02465cf
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-06 16:22:32 -0700

    feat: add post product routes

--- src/modules/products/productRoutes.ts
+++ src/modules/products/productRoutes.ts
@@ -1,8 +1,12 @@
 import {Router} from "express";
-import {getProcuts, getProduct} from "./productController.js";
+import {getProcuts, getProduct, postProduct} from "./productController.js";
+import {createProductSchema} from "./productSchema.js";
+import {validate} from "../../middlewares/validate.js";
+
 const router = Router();
 
 router.get("/",getProcuts);
 router.get("/:id",getProduct);
+router.post("/",validate(createProductSchema),postProduct);
 
 export default router;
