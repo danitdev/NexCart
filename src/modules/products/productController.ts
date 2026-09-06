55c0a222a7b953026a8cad55045ad5d4e1cc1c00
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-06 16:22:19 -0700

    feat: add post product to controller

--- src/modules/products/productController.ts
+++ src/modules/products/productController.ts
@@ -1,7 +1,7 @@
 import {Request,Response,NextFunction} from "express";
-import { getProductService, getProductsService } from "./productService.js";
+import { getProductService, getProductsService, postProductService } from "./productService.js";
 import { AppError } from "../../errors/AppError.js";
-
+import {CreateProducInput} from "./productSchema.js";
 
 
 export const getProcuts = async(
@@ -29,6 +29,32 @@ export const getProduct = async(
             const productId = Number(req.params.id);
             const product = await getProductService(productId);
             res.status(200).json({product:product});
+        }catch(err){
+            if(err instanceof AppError){
+                if(!err.statusCode){
+                    err.statusCode = 500;
+                }
+            }
+            next(err);
+        }
+}
+export const postProduct = async(
+    req:Request,
+    res:Response,
+    next:NextFunction)=>{
+        try{
+            const data:CreateProducInput = {
+                categoryId:1,
+                name:req.body.name,
+                price:req.body.price,
+                stock:req.body.stock,
+                description:req.body.description,
+                
+            }
+
+            // TODO: uploading the image and set the image url
+            const product = await postProductService(data);
+            res.status(201).json({product,msg:"product created."});
         }catch(err){
             if(err instanceof AppError){
                 if(!err.statusCode){
