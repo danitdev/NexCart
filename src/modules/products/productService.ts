3d69bb67350151ef8f4f8b2a80ae501a2f0f9821
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-06 16:22:43 -0700

    feat: add post product service

--- src/modules/products/productService.ts
+++ src/modules/products/productService.ts
@@ -1,7 +1,7 @@
 import {prisma} from "../../lib/prisma.js";
 import {AppError} from "../../errors/AppError.js";
 import {Prisma} from "../../generated/prisma/client.js";
-
+import {CreateProducInput} from "./productSchema.js";
 
 export const getProductsService = async()=>{
     return await prisma.product.findMany();
@@ -11,5 +11,17 @@ export const getProductService = async(productId:number)=>{
     if(!product){
         throw new AppError("this product doesn't exist",404);
     }
+    return product;
+}
+
+export const postProductService = async(data:CreateProducInput)=>{
+    const product = await prisma.product.create({data:{
+        name:data.name,
+        price:data.price,
+        categoryId:1,
+        description:data.description,
+        imageUrl:"/test-URL",  // TODO: have to make the products be able to upload some
+        stock: data.stock
+    }});
     return product;
 }
