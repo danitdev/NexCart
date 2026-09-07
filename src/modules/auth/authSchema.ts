c8fa49ff6beda72de1407599199c794727551719
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-06 18:29:25 -0700

    feat: check for the confirm password

--- src/modules/auth/authSchema.ts
+++ src/modules/auth/authSchema.ts
@@ -3,8 +3,18 @@ import {z} from "zod";
 export const createUserSchema = z.object({
     name: z.string().trim().min(2).max(100).optional(),
     email: z.email().trim().toLowerCase(),
-    password: z.string().min(8).max(255),
-});
+    password: z.string().min(8).max(255)
+        .regex(/[A-Z]/,"Password must contain at least one uppercase letter.")
+        .regex(/[a-z]/,"Password must contain at least one lowercase letter.")
+        .regex(/[0-9]/,"Password must contain at least one number.")
+        .regex(/[^A-Za-z0-9]/,"Password must contain at least one special character"),
+    confirmPassword: z.string()
+}).refine(
+    data=> data.password === data.confirmPassword,{
+        message:"Passwords do not match.",
+        path:["confirmPassword"]
+    }
+)
 
 
 export type CreateUserInput = z.infer<typeof createUserSchema>;
