667d48676d405a6f2efd57867e22df7322aad03a
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-07 14:01:39 -0700

    feat: add login routes

--- src/modules/auth/authRoutes.ts
+++ src/modules/auth/authRoutes.ts
@@ -1,11 +1,11 @@
 import {Router} from "express";
 import {validate} from "../../middlewares/validate.js";
-import {createUserSchema} from "./authSchema.js";
-
-import {signUp} from "./authController.js";
+import {createUserSchema, loginUserSchema} from "./authSchema.js";
+import {login, signUp} from "./authController.js";
 
 const router = Router();
 
 router.post("/signup",validate(createUserSchema),signUp)
+router.post("/login",validate(loginUserSchema),login);
 
 export default router;
