57d9d330fdec3a531b78c81545e7ce1d85c6d827
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-07 14:03:22 -0700

    feat: add auth and admin middleware to protected route

--- src/modules/categories/categoryRoutes.ts
+++ src/modules/categories/categoryRoutes.ts
@@ -1,13 +1,14 @@
 import {Router} from "express";
 import {deleteCategory, getCategories, getCategory, postCategory, updateCategory} from "./categoryController.js";
-
+import {isAuth} from "../../middlewares/isAuth.js";
+import {isAdmin} from "../../middlewares/isAdmin.js";
 const router = Router();
 
 router.get("/",getCategories);
 router.get("/:id",getCategory);
-router.post("/",postCategory);
-router.patch("/:id",updateCategory);
-router.delete("/:id",deleteCategory);
+router.post("/",isAuth,isAdmin,isAuth,postCategory);
+router.patch("/:id",isAuth,isAdmin,isAuth,updateCategory);
+router.delete("/:id",isAuth,isAdmin,isAuth,deleteCategory);
 
 
 export default router;
--- src/modules/products/productRoutes.ts
+++ src/modules/products/productRoutes.ts
@@ -2,14 +2,15 @@ import {Router} from "express";
 import {deleteProduct, getProcuts, getProduct, patchProduct, postProduct} from "./productController.js";
 import {createProductSchema, updateProductSchema} from "./productSchema.js";
 import {validate} from "../../middlewares/validate.js";
-import de from "zod/v4/locales/de.cjs";
+import {isAuth} from "../../middlewares/isAuth.js";
+import {isAdmin} from "../../middlewares/isAdmin.js";
 
 const router = Router();
 
 router.get("/",getProcuts);
 router.get("/:id",getProduct);
-router.post("/",validate(createProductSchema),postProduct);
-router.patch("/:id",validate(updateProductSchema),patchProduct);
-router.delete("/:id",deleteProduct);
+router.post("/",isAuth,isAdmin,validate(createProductSchema),postProduct);
+router.patch("/:id",isAuth,isAdmin,validate(updateProductSchema),patchProduct);
+router.delete("/:id",isAuth,isAdmin,deleteProduct);
 
 export default router;
