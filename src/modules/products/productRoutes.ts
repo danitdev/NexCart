f381babe3e535e7498c0d485ca7e8ae10630f72a
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-06 17:40:58 -0700

    feat: add delete routes

--- src/modules/products/productRoutes.ts
+++ src/modules/products/productRoutes.ts
@@ -1,13 +1,15 @@
 import {Router} from "express";
-import {getProcuts, getProduct, patchProduct, postProduct} from "./productController.js";
-import {createProductSchema} from "./productSchema.js";
+import {deleteProduct, getProcuts, getProduct, patchProduct, postProduct} from "./productController.js";
+import {createProductSchema, updateProductSchema} from "./productSchema.js";
 import {validate} from "../../middlewares/validate.js";
+import de from "zod/v4/locales/de.cjs";
 
 const router = Router();
 
 router.get("/",getProcuts);
 router.get("/:id",getProduct);
 router.post("/",validate(createProductSchema),postProduct);
-router.patch("/:id",validate(createProductSchema),patchProduct);
+router.patch("/:id",validate(updateProductSchema),patchProduct);
+router.delete("/:id",deleteProduct);
 
 export default router;
