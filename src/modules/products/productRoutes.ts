6ce127d386fe147a3b466b00eb6ca40bd814a1ce
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-07 14:17:31 -0700

    feat: add available products

--- src/modules/products/productRoutes.ts
+++ src/modules/products/productRoutes.ts
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {deleteProduct, getProcuts, getProduct, patchProduct, postProduct} from "./productController.js";
+import {deleteProduct, getProcuts, getProcutsAvailable, getProduct, patchProduct, postProduct} from "./productController.js";
 import {createProductSchema, updateProductSchema} from "./productSchema.js";
 import {validate} from "../../middlewares/validate.js";
 import {isAuth} from "../../middlewares/isAuth.js";
@@ -8,6 +8,8 @@ import {isAdmin} from "../../middlewares/isAdmin.js";
 const router = Router();
 
 router.get("/",getProcuts);
+//returns product with more than 0 stock
+router.get("/available",getProcutsAvailable);
 router.get("/:id",getProduct);
 router.post("/",isAuth,isAdmin,validate(createProductSchema),postProduct);
 router.patch("/:id",isAuth,isAdmin,validate(updateProductSchema),patchProduct);
