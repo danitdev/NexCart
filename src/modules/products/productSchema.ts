1f3c2e529fd4f4ff76b93176293ec6cea9386962
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-07 16:52:38 -0700

    feat: convert price stock and categoryId if received by multipart/form-data

--- src/modules/products/productSchema.ts
+++ src/modules/products/productSchema.ts
@@ -1,22 +1,22 @@
 import {z} from "zod";
 
 export const createProductSchema = z.object({
-    price: z.number().nonnegative().max(99999999.99).multipleOf(0.01),
+    price: z.coerce.number().nonnegative().max(99999999.99).multipleOf(0.01),
     name: z.string().trim().min(1).max(255),
     description: z.string().trim().max(2000).optional(),
-    stock: z.number().int().nonnegative().default(0),
+    stock: z.coerce.number().int().nonnegative().default(0),
     imageUrl: z.string().url().optional(),
-    categoryId: z.number().int().positive()
+    categoryId: z.coerce.number().int().positive()
 
 });
 
 export const updateProductSchema = z.object({
-    price: z.number().nonnegative().max(99999999.99).multipleOf(0.01).optional(),
+    price: z.coerce.number().nonnegative().max(99999999.99).multipleOf(0.01).optional(),
     name: z.string().trim().max(255).optional(),
     description: z.string().trim().max(2000).optional(),
-    stock: z.number().int().nonnegative().optional(),
+    stock: z.coerce.number().int().nonnegative().optional(),
     imageUrl: z.string().url().optional(),
-    categoryId: z.number().int().positive().optional()
+    categoryId: z.coerce.number().int().positive().optional()
 
     // USING refine to tell the customer to update at least one field
 }).refine(data=>Object.keys(data).length>0,{message:"at least one field must be provided"});
