baffbcd991adc7a4519d1447c273a068575096d8
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-09 14:08:06 -0700

    fix: empty the cart after order created

--- src/modules/orders/orderService.ts
+++ src/modules/orders/orderService.ts
@@ -1,85 +1,79 @@
-import {prisma} from "../../lib/prisma.js";
-import {AppError} from "../../errors/AppError.js";
-import {Prisma} from "../../generated/prisma/client.js";
-<<<<<<< HEAD
-import { OrderItemScalarFieldEnum } from "../../generated/prisma/internal/prismaNamespace.js";
-=======
->>>>>>> 7eee12a62993f9f48ed82debcd231c1a4b309f3c
+import { prisma } from "../../lib/prisma.js";
+import { AppError } from "../../errors/AppError.js";
+import { Prisma } from "../../generated/prisma/client.js";
 
-export const checkoutCartService = async(userId:number)=>{
-    let totalCost = 0;
-    //wrap the whole thing in transaction so if it failed it rollbacks all the things
-<<<<<<< HEAD
-    const order = await prisma.$transaction(async(tx)=>{
-=======
-    await prisma.$transaction(async(tx)=>{
->>>>>>> 7eee12a62993f9f48ed82debcd231c1a4b309f3c
-        //finding the user cart
+export const checkoutCartService = async (userId: number) => {
+    // wrap the whole thing in transaction so if it failed it rolls back all the things
+    const order = await prisma.$transaction(async (tx) => {
+        // finding the user cart
         const userCart = await tx.cart.findUnique({
-            where:{userId},
-            include:{
-                items:{
-                    include:{
-                        product:{
-                            select:{
-<<<<<<< HEAD
-                                price:true,
-                                stock:true,
-                                name:true
-=======
-                                price:true
->>>>>>> 7eee12a62993f9f48ed82debcd231c1a4b309f3c
+            where: { userId },
+            include: {
+                items: {
+                    include: {
+                        product: {
+                            select: {
+                                price: true,
+                                stock: true,
+                                name: true
                             }
                         }
                     }
                 }
-            }});
-        if(!userCart){
-            throw new AppError("User cart doesn't exist.",404);
-        }
-        //check for cart being empty
-        if(userCart.items.length === 0){
-            throw new AppError("Cart is empty",400);
-        }
-        //calculate total cost
-        userCart.items.forEach(item=>{
-            totalCost += Number(item.product.price) * item.quantity
-        })
-        const order = await tx.order.create({data:{totalAmount:totalCost,userId},select:{id:true}});
-        for(const item of userCart.items){
-<<<<<<< HEAD
-            //check for enough stocks
-            if(item.product.stock<item.quantity){
-                throw new AppError(`Not enough stock for ${item.product.name}`,400);
             }
-            //subtract the quantity
+        });
+        if (!userCart) {
+            throw new AppError("User cart doesn't exist.", 404);
+        }
+        // check for cart being empty
+        if (userCart.items.length === 0) {
+            throw new AppError("Cart is empty", 400);
+        }
+        // calculate total cost
+        let totalCost = 0;
+        userCart.items.forEach(item => {
+            totalCost += Number(item.product.price) * item.quantity;
+        });
+        const order = await tx.order.create({
+            data: {
+                totalAmount: totalCost,
+                userId
+            },
+            select: {
+                id: true
+            }
+        });
+        for (const item of userCart.items) {
+            // check for enough stocks
+            if (item.product.stock < item.quantity) {
+                throw new AppError(
+                    `Not enough stock for ${item.product.name}`,
+                    400
+                );
+            }
+            // subtract the quantity
             await tx.product.update({
-                where:{
-                    id:item.productId
+                where: {
+                    id: item.productId
                 },
-                data:{
-                    stock:{
-                        decrement:item.quantity
+                data: {
+                    stock: {
+                        decrement: item.quantity
                     }
                 }
             });
-
-=======
->>>>>>> 7eee12a62993f9f48ed82debcd231c1a4b309f3c
-            await tx.orderItem.create(
-                {data:{
-                    price:item.product.price,
-                    quantity:item.quantity,
-                    orderId:order.id,
-                    productId:item.productId
-                }})
+            await tx.orderItem.create({
+                data: {
+                    price: item.product.price,
+                    quantity: item.quantity,
+                    orderId: order.id,
+                    productId: item.productId
+                }
+            });
         }
+        //emptying the cart
+        await tx.cartItem.deleteMany({where:{cartId:userCart.id}});
         return order;
     });
-<<<<<<< HEAD
     return order;
-=======
-
->>>>>>> 7eee12a62993f9f48ed82debcd231c1a4b309f3c
-
-}
+};
