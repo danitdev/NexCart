8ba038d2751b3dd49c27c36904708319fc6aa2a3
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-08 19:38:16 -0700

    feat: add addToCart to controller

--- src/modules/carts/cartController.ts
+++ src/modules/carts/cartController.ts
@@ -1,5 +1,5 @@
 import type {Request,Response,NextFunction} from "express";
-import { getCartService } from "./cartService.js";
+import { addToCartService, getCartService } from "./cartService.js";
 import {} from "./cartSchema.js";
 import {AppError} from "../../errors/AppError.js";
 
@@ -11,6 +11,25 @@ export const getCart = async(
         try{
             const cart = await getCartService(req.userId!);
             res.status(200).json({cart})
+        }catch(err){
+            if(err instanceof AppError){
+                if(!err.statusCode){
+                    err.statusCode = 500;
+                }
+            }
+            next(err);
+        }
+}
+
+export const addToCart = async(
+    req:Request,
+    res:Response,
+    next:NextFunction)=>{
+        try{
+            const productId = Number(req.body.productId);
+            const quantity =Number(req.body.quantity);
+            const cartItem = await addToCartService(req.userId!,productId,quantity);
+            res.status(200).json({cartItem,msg:"added item to the cart"})
         }catch(err){
             if(err instanceof AppError){
                 if(!err.statusCode){
