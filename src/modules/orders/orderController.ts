5b11bfbc6e1dfc98b6c25b6fef8007d15b9dc5ec
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-09 13:54:57 -0700

    feat: add checkout cart to order

---
+++ src/modules/orders/orderController.ts
@@ -0,0 +1,40 @@
+import type {Request,Response,NextFunction} from "express";
+import { AppError } from "../../errors/AppError.js";
+import {parseId} from "../../utils/parseId.js";
+import { checkoutCartService } from "./orderService.js";
+
+
+export const getOrders = async(
+    req:Request,
+    res:Response,
+    next:NextFunction)=>{
+        try{
+
+            res.status(200).json({});
+        }catch(err){
+            if(err instanceof AppError){
+                if(!err.statusCode){
+                    err.statusCode = 500;
+                }
+            }
+            next(err);
+        }
+
+};
+export const checkoutCartToOrder = async(
+    req:Request,
+    res:Response,
+    next:NextFunction)=>{
+        try{
+            const order = await checkoutCartService(req.userId!);
+            res.status(200).json({msg:"order checkout.",order});
+        }catch(err){
+            if(err instanceof AppError){
+                if(!err.statusCode){
+                    err.statusCode = 500;
+                }
+            }
+            next(err);
+        }
+
+};
