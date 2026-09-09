ebbd597054e4070c78becaa9ea265a78180b3ed9
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-08 19:38:29 -0700

    feat: add addToCart to router

--- src/modules/carts/cartRoutes.ts
+++ src/modules/carts/cartRoutes.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import {validate} from "../../middlewares/validate.js";
-import { getCart } from "./cartController.js";
+import { addToCart, getCart } from "./cartController.js";
 import {isAdmin} from "../../middlewares/isAdmin.js";
 import {isAuth} from "../../middlewares/isAuth.js";
 const router = Router();
 
 router.get("/",isAuth,getCart);
+router.post("/items",isAuth,addToCart);
 
 export default router;
