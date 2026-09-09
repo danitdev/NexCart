0e0bd108daf45c42003be3634421feb622955470
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-08 20:04:17 -0700

    feat: add update cart to router

--- src/modules/carts/cartRoutes.ts
+++ src/modules/carts/cartRoutes.ts
@@ -1,11 +1,12 @@
 import {Router} from "express";
 import {validate} from "../../middlewares/validate.js";
-import { addToCart, getCart } from "./cartController.js";
+import { addToCart, getCart, updateCart } from "./cartController.js";
 import {isAdmin} from "../../middlewares/isAdmin.js";
 import {isAuth} from "../../middlewares/isAuth.js";
 const router = Router();
 
 router.get("/",isAuth,getCart);
 router.post("/items",isAuth,addToCart);
+router.patch("/items/:itemId",isAuth,updateCart);
 
 export default router;
