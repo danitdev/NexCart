a41b94ae965ed3e65b8b1c5c2407208f9b22f2cb
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-08 20:04:27 -0700

    feat: add update cart service

--- src/modules/carts/cartService.ts
+++ src/modules/carts/cartService.ts
@@ -1,6 +1,7 @@
 import {prisma} from "../../lib/prisma.js";
 import {AppError} from "../../errors/AppError.js";
 import {Prisma} from "../../generated/prisma/client.js";
+import { CartItemScalarFieldEnum, UserScalarFieldEnum } from "../../generated/prisma/internal/prismaNamespace.js";
 
 
 
@@ -65,6 +66,31 @@ export const addToCartService = async(userId:number,productId:number,quantity:nu
         });
     }
     catch(err){
+        throw err;
+    }
+}
+export const updateCartService = async(itemId:number,userId:number,quantity:number)=>{
+    try{
+        if(quantity ===0){
+            throw new AppError("Quantity should be more that 0",400);
+        }
+        const cart =await prisma.cart.findUnique({where:{userId}});
+        if(!cart){
+            throw new AppError("Cart not found.",404);
+        }
+        const existingItem = await prisma.cartItem.findFirst({where:{id:itemId,cartId:cart.id}});
+        if(!existingItem){
+            throw new AppError("This item doesn't exist in your cart.",404);
+        }
+        const productOfItem = await prisma.product.findUnique({where:{id:existingItem.productId}});
+        if(!productOfItem){
+            throw new AppError("this item doesn't exist.",404)
+        }
+        if(quantity > productOfItem.stock ){
+            throw new AppError("Not enough stock.",400);
+        }
+        return await prisma.cartItem.update({where:{id :itemId},data:{quantity}});
+    }catch(err){
         throw err;
     }
 }
