dd9d960d1696f005fedc0f62a0b33b459dbd10d6
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-09 12:25:54 -0700

    feat: add delete route for cart router

--- src/modules/carts/cartRoutes.ts
+++ src/modules/carts/cartRoutes.ts
@@ -1,6 +1,6 @@
 import {Router} from "express";
 import {validate} from "../../middlewares/validate.js";
-import { addToCart, getCart, updateCart } from "./cartController.js";
+import { addToCart, deleteItemFromCart, getCart, updateCart } from "./cartController.js";
 import {isAdmin} from "../../middlewares/isAdmin.js";
 import {isAuth} from "../../middlewares/isAuth.js";
 const router = Router();
@@ -8,5 +8,6 @@ const router = Router();
 router.get("/",isAuth,getCart);
 router.post("/items",isAuth,addToCart);
 router.patch("/items/:itemId",isAuth,updateCart);
+router.delete("/items/:itemId",isAuth,deleteItemFromCart);
 
 export default router;
