64b3562b3a89efb4d661ba1a0719187153bba78c
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-13 03:52:40 -0700

    feat: add post review route

---
+++ src/modules/reviews/reviewRoutes.ts
@@ -0,0 +1,11 @@
+import {Router} from "express";
+import {isAuth} from "../../middlewares/isAuth.js";
+import {postReviewSchema} from "./reviewSchema.js";
+import {validate} from "../../middlewares/validate.js";
+import { postReview } from "./reviewController.js";
+
+const router = Router();
+
+router.post("/products/:id/",isAuth,validate(postReviewSchema),postReview);
+// router.get("/products/:id/");
+export default router;
