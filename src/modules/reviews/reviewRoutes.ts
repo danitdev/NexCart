2f22882cfe1a8dea81289739d0073e6854a5c784
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-13 04:23:14 -0700

    feat: add update review route

--- src/modules/reviews/reviewRoutes.ts
+++ src/modules/reviews/reviewRoutes.ts
@@ -2,10 +2,11 @@ import {Router} from "express";
 import {isAuth} from "../../middlewares/isAuth.js";
 import {postReviewSchema} from "./reviewSchema.js";
 import {validate} from "../../middlewares/validate.js";
-import { postReview } from "./reviewController.js";
+import { deleteReview, postReview, updateReview } from "./reviewController.js";
 
 const router = Router();
 
 router.post("/products/:id",isAuth,validate(postReviewSchema),postReview);
-router.delete("/products/:id",isAuth,);
+router.delete("/products/:id",isAuth,deleteReview);
+router.patch("/products/:id",isAuth,updateReview);
 export default router;
