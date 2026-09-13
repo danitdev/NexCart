13efdceee9f1db30fdfc4d612fe6ccb305f20c0f
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-13 04:09:58 -0700

    feat: add delete review service and check for existing review on post review

--- src/modules/reviews/reviewService.ts
+++ src/modules/reviews/reviewService.ts
@@ -10,6 +10,16 @@ export const postReviewService = async(userId:number,productId:number,comment:st
     if(!product){
         throw new AppError("Product not found.",404);
     }
+    const existingReview = await prisma.review.findUnique({
+        where:{
+            userId_productId:{
+                productId,userId
+            }
+        }
+    })
+    if(existingReview){
+        throw new AppError("You have already reviewed this product",400);
+    }
     const review = await prisma.review.create({
         data:{
             userId,
@@ -19,4 +29,28 @@ export const postReviewService = async(userId:number,productId:number,comment:st
         }
     });
     return review;
+}
+
+export const deleteReviewService = async(userId:number,productId:number)=>{
+    const product = await prisma.product.findUnique({
+        where:{id:productId}
+    });
+    if(!product){
+        throw new AppError("Product not found.",404);
+    }
+    const review = await prisma.review.findUnique({
+        where:{userId_productId:{
+            productId:productId,
+            userId:userId
+        }}
+    });
+    if(!review){
+        throw new AppError("There is no review from this user on this product",400);
+    }
+    await prisma.review.delete({
+        where:{userId_productId:{
+            userId,
+            productId
+        }}
+    });
 }
