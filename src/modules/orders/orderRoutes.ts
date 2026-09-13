6cb9eeca9525fe88eeafbbba23fff8a0e477de74
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-13 02:33:55 -0700

    feat: add get order by id admin to routes

--- src/modules/orders/orderRoutes.ts
+++ src/modules/orders/orderRoutes.ts
@@ -1,17 +1,19 @@
 import {Router} from "express";
 import { isAuth } from "../../middlewares/isAuth.js";
 import {isAdmin} from "../../middlewares/isAdmin.js";
-import { checkoutCartToOrder, getOrderById, getOrders, updateOrderStatusByAdmin } from "./orderController.js";
+import { checkoutCartToOrder, getOrderById, getOrderByIdAdmin, getOrders, getOrdersAdmin, updateOrderStatusByAdmin } from "./orderController.js";
 import {updateOrderStatusSchema} from "./orderSchema.js";
 import {validate} from "../../middlewares/validate.js";
+
 
 const router = Router();
 
 router.post("/checkout",isAuth,checkoutCartToOrder);
-router.get("/",getOrders);
-router.get("/:id",getOrderById);
-// router.get("/admin",isAuth,isAdmin);
-// router.get("/admin/:id",isAuth,isAdmin);
+router.get("/admin",isAuth,isAdmin,getOrdersAdmin);
 router.patch("/admin/:id/status",isAuth,isAdmin,validate(updateOrderStatusSchema),updateOrderStatusByAdmin);
+router.get("/admin/:id",isAuth,isAdmin,getOrderByIdAdmin);
+router.get("/",isAuth,getOrders);
+router.get("/:id",isAuth,getOrderById);
+
 
 export default router;
