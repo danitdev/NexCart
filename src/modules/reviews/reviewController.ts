67b67aa67229c0ff560837890a08b966a288d8ed
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-13 04:23:33 -0700

    feat: add update review to controller

--- src/modules/reviews/reviewController.ts
+++ src/modules/reviews/reviewController.ts
@@ -1,7 +1,8 @@
 import type {Request,Response,NextFunction} from "express";
 import { AppError } from "../../errors/AppError.js";
 import {parseId} from "../../utils/parseId.js";
-import {deleteReviewService, postReviewService} from "./reviewService.js";
+import {deleteReviewService, postReviewService, updateReviewService} from "./reviewService.js";
+import { UpdateReview } from "./reviewSchema.js";
 
 export const postReview = async(
     req:Request,
@@ -31,6 +32,26 @@ export const deleteReview = async(
             const productId = parseId(req.params.id);
             await deleteReviewService(req.userId!,productId);
             res.status(204).send();
+        }catch(err){
+            if(err instanceof AppError){
+                if(!err.statusCode){
+                    err.statusCode = 500;
+                }
+            }
+            next(err);
+        }
+
+};
+
+export const updateReview = async(
+    req:Request,
+    res:Response,
+    next:NextFunction)=>{
+        try{
+            const productId = parseId(req.params.id);
+            const data:UpdateReview = req.body;
+            const updatedReview = await updateReviewService(req.userId!,productId,data);
+             res.status(200).json({updatedReview,msg:"Review updated!"});
         }catch(err){
             if(err instanceof AppError){
                 if(!err.statusCode){
