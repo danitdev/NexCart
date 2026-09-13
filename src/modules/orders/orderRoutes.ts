756608ba2c4664ff6c0c83e34d94e34b58c01698
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-13 01:47:05 -0700

    feat: add get order by id route

--- src/modules/orders/orderRoutes.ts
+++ src/modules/orders/orderRoutes.ts
@@ -1,11 +1,12 @@
 import {Router} from "express";
 import { isAuth } from "../../middlewares/isAuth.js";
-import { checkoutCartToOrder, getOrders } from "./orderController.js";
+import { checkoutCartToOrder, getOrderById, getOrders } from "./orderController.js";
 
 
 const router = Router();
 
 router.post("/checkout",isAuth,checkoutCartToOrder);
 router.get("/",getOrders);
+router.get("/:id",getOrderById);
 
 export default router;
