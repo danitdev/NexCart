3540e4176592c4fbb17a1cae504acc7bbb4c39c7
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-13 04:23:46 -0700

    feat: add update review service

--- src/modules/reviews/reviewService.ts
+++ src/modules/reviews/reviewService.ts
@@ -1,7 +1,8 @@
 import {prisma} from "../../lib/prisma.js";
 import {AppError} from "../../errors/AppError.js";
 import {Prisma} from "../../generated/prisma/client.js";
-
+import {UpdateReview} from "./reviewSchema.js";
+import { updateReview } from "./reviewController.js";
 
 export const postReviewService = async(userId:number,productId:number,comment:string,rating:number)=>{
     const product = await prisma.product.findUnique({
@@ -53,4 +54,33 @@ export const deleteReviewService = async(userId:number,productId:number)=>{
             productId
         }}
     });
+}
+
+
+export const updateReviewService = async(userId:number,productId:number,data:UpdateReview)=>{
+    try{
+
+        const product = await prisma.product.findUnique({
+            where:{id:productId}
+        });
+        if(!product){
+            throw new AppError("Product not found.",404);
+        }
+        const updatedReview = await prisma.review.update({
+            data,
+            where:{
+                userId_productId:{
+                    userId,
+                    productId
+                }
+            }
+        });
+        return updatedReview;
+    }
+    catch(error){
+        if(error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2025"){
+            throw new AppError("Review not found.",404);
+        }
+        throw error;
+    }
 }
