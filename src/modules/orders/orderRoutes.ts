872ada44aea7f1c1f5984218210435ee8df31989
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-13 02:08:58 -0700

    feat: add updateorder status route

--- src/modules/orders/orderRoutes.ts
+++ src/modules/orders/orderRoutes.ts
@@ -1,12 +1,17 @@
 import {Router} from "express";
 import { isAuth } from "../../middlewares/isAuth.js";
-import { checkoutCartToOrder, getOrderById, getOrders } from "./orderController.js";
-
+import {isAdmin} from "../../middlewares/isAdmin.js";
+import { checkoutCartToOrder, getOrderById, getOrders, updateOrderStatusByAdmin } from "./orderController.js";
+import {updateOrderStatusSchema} from "./orderSchema.js";
+import {validate} from "../../middlewares/validate.js";
 
 const router = Router();
 
 router.post("/checkout",isAuth,checkoutCartToOrder);
 router.get("/",getOrders);
 router.get("/:id",getOrderById);
+// router.get("/admin",isAuth,isAdmin);
+// router.get("/admin/:id",isAuth,isAdmin);
+router.patch("/admin/:id/status",isAuth,isAdmin,validate(updateOrderStatusSchema),updateOrderStatusByAdmin);
 
 export default router;
