13a7668faf76cf40ddd2e3d31e2312c4780ee67a
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-13 01:47:18 -0700

    feat: add get order by id to controller

--- src/modules/orders/orderController.ts
+++ src/modules/orders/orderController.ts
@@ -1,7 +1,7 @@
 import type {Request,Response,NextFunction} from "express";
 import { AppError } from "../../errors/AppError.js";
 import {parseId} from "../../utils/parseId.js";
-import { checkoutCartService, getOrdersService } from "./orderService.js";
+import { checkoutCartService, getOrderByIdService, getOrdersService } from "./orderService.js";
 
 
 export const getOrders = async(
@@ -11,6 +11,24 @@ export const getOrders = async(
         try{
             const orders = await getOrdersService(req.userId!);
             res.status(200).json({orders});
+        }catch(err){
+            if(err instanceof AppError){
+                if(!err.statusCode){
+                    err.statusCode = 500;
+                }
+            }
+            next(err);
+        }
+
+};
+export const getOrderById = async(
+    req:Request,
+    res:Response,
+    next:NextFunction)=>{
+        try{
+            const orderId = Number(req.params.id);
+            const order = await getOrderByIdService(req.userId!,orderId);
+            res.status(200).json({order});
         }catch(err){
             if(err instanceof AppError){
                 if(!err.statusCode){
