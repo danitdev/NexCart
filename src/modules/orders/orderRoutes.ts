9fe62d44ee93825b87c49b42b3e60a0b0915d823
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-13 01:39:50 -0700

    feat: get orders route

--- src/modules/orders/orderRoutes.ts
+++ src/modules/orders/orderRoutes.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import { isAuth } from "../../middlewares/isAuth.js";
-import { checkoutCartToOrder } from "./orderController.js";
+import { checkoutCartToOrder, getOrders } from "./orderController.js";
 
 
 const router = Router();
 
 router.post("/checkout",isAuth,checkoutCartToOrder);
+router.get("/",getOrders);
 
 export default router;
