faf88f170bdf772d5bc3276462bbeb0e929eeb1d
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-13 04:23:01 -0700

    feat: add type for update review

--- src/modules/reviews/reviewSchema.ts
+++ src/modules/reviews/reviewSchema.ts
@@ -5,3 +5,7 @@ export const postReviewSchema = z.object({
     rating: z.coerce.number().int().min(1).max(5),
     comment: z.string().trim().min(1).max(1000)
 })
+
+
+
+export type UpdateReview = z.infer<typeof postReviewSchema>;
