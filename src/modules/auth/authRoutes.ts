07d37b0e6c17d2ea524a0bd9d58d10f84108f835
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-19 04:26:49 -0700

    feat: add forgot password route

--- src/modules/auth/authRoutes.ts
+++ src/modules/auth/authRoutes.ts
@@ -1,11 +1,13 @@
 import {Router} from "express";
 import {validate} from "../../middlewares/validate.js";
 import {createUserSchema, loginUserSchema} from "./authSchema.js";
-import {login, signUp} from "./authController.js";
+import {forgotPassword, login, signUp} from "./authController.js";
 
 const router = Router();
 
 router.post("/signup",validate(createUserSchema),signUp)
 router.post("/login",validate(loginUserSchema),login);
+router.post("/forgot-password",forgotPassword);
+
 
 export default router;
