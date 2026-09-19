a26308b93f4fe9d5cf68ec610fef05aa8f280343
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-19 04:26:34 -0700

    feat:: add forgot password to auth controller

--- src/modules/auth/authController.ts
+++ src/modules/auth/authController.ts
@@ -1,5 +1,5 @@
 import type {Request,Response,NextFunction} from "express";
-import {loginUserService, signupUserService} from "./authService.js";
+import {forgotPasswordService, loginUserService, signupUserService} from "./authService.js";
 import {CreateUserInput,LoginUserInput} from "./authSchema.js";
 import {AppError} from "../../errors/AppError.js";
 
@@ -29,6 +29,24 @@ export const login = async(
             const data:LoginUserInput = req.body;
             const {token,userId} = await loginUserService(data);
             res.status(200).json({token,userId}); 
+        }catch(err){
+            if(err instanceof AppError){
+                if(!err.statusCode){
+                    err.statusCode = 500;
+                }
+            }
+            next(err);
+        }
+}
+
+export const forgotPassword = async(
+    req:Request,
+    res:Response,
+    next:NextFunction)=>{
+        try{   
+            const email = req.body.email;
+            const {message,token} = await forgotPasswordService(email);
+            res.status(200).json({message,token}); 
         }catch(err){
             if(err instanceof AppError){
                 if(!err.statusCode){
