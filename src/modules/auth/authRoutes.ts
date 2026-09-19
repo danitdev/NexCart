ee1a5e6f767c367a61aceaa854490e6c63d524ac
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-19 04:45:16 -0700

    feat: add reset password route

--- src/modules/auth/authRoutes.ts
+++ src/modules/auth/authRoutes.ts
@@ -1,13 +1,13 @@
 import {Router} from "express";
 import {validate} from "../../middlewares/validate.js";
 import {createUserSchema, loginUserSchema} from "./authSchema.js";
-import {forgotPassword, login, signUp} from "./authController.js";
+import {forgotPassword, login, resetPassword, signUp} from "./authController.js";
 
 const router = Router();
 
 router.post("/signup",validate(createUserSchema),signUp)
 router.post("/login",validate(loginUserSchema),login);
 router.post("/forgot-password",forgotPassword);
-
+router.post("/reset-password",resetPassword);
 
 export default router;
