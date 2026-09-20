234619144e2e31240a3ddc6f17b14668c142ac7f
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-09-19 17:26:42 -0700

    feat: validate the query for get products route

--- src/modules/products/productRoutes.ts
+++ src/modules/products/productRoutes.ts
@@ -1,13 +1,13 @@
 import {Router} from "express";
 import {deleteProduct, getProcuts, getProduct, patchProduct, postProduct} from "./productController.js";
-import {createProductSchema, updateProductSchema} from "./productSchema.js";
-import {validate} from "../../middlewares/validate.js";
+import {createProductSchema, productQuerySchema, updateProductSchema} from "./productSchema.js";
+import {validate,validateQuery} from "../../middlewares/validate.js";
 import {isAuth} from "../../middlewares/isAuth.js";
 import {isAdmin} from "../../middlewares/isAdmin.js";
 
 const router = Router();
 
-router.get("/",getProcuts);
+router.get("/",validateQuery(productQuerySchema),getProcuts);
 router.get("/:id",getProduct);
 router.post("/",isAuth,isAdmin,validate(createProductSchema),postProduct);
 router.patch("/:id",isAuth,isAdmin,validate(updateProductSchema),patchProduct);
